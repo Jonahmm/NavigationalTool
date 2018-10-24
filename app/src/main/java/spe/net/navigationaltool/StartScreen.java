a2866af3d97fcf156bf5a499c3ede7fa682c5870
Jonahmm <jm17402@my.bristol.ac.uk>
2018-10-24 10:53:22 +0100

    Initialised Android studio project

---
+++ app/src/main/java/spe/net/navigationaltool/StartScreen.java
@@ -0,0 +1,13 @@
+package spe.net.navigationaltool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+}
