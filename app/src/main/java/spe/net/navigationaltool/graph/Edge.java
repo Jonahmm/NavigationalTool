27bb5b18485bd53ab3bb54e0920827914b83df8c
Laurence <hii488@btinternet.com>
2018-10-24 12:35:39 +0100

    Added length & direction to Edge

--- app/src/main/java/spe/net/navigationaltool/graph/Edge.java
+++ app/src/main/java/spe/net/navigationaltool/graph/Edge.java
@@ -1,9 +1,14 @@
 package spe.net.navigationaltool.graph;
 
+import android.graphics.Point;
+
 public class Edge {
 
     public Node nodeA;
     public Node nodeB;
+
+    public int length;
+    public Point direction;
 
     public boolean disabled;
     public AccessLevel accessLevel;
@@ -12,6 +17,11 @@ public class Edge {
         this.nodeA = a;
         this.nodeB = b;
 
+        // Assuming straight line:
+        length = (int) Math.sqrt(Math.pow(nodeA.location.x - nodeB.location.x, 2) + Math.pow(nodeA.location.y - nodeB.location.y, 2));
+        // This is undirected, so use backwards too.
+        direction = new Point(nodeA.location.x - nodeB.location.x, nodeA.location.y - nodeB.location.y);
+        
         disabled = true; // Just as an assumption, most routes are corridors.
         accessLevel = AccessLevel.STUDENTCARD; // As an assumption, most places will require a ucard to get to.
     }
