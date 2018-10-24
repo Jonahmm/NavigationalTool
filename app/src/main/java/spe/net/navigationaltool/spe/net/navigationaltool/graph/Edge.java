51208e42b4be5f137316c721e91a5e3578a91297
Laurence <hii488@btinternet.com>
2018-10-24 11:38:50 +0100

    Added constructors & Edge attributes
    Constructors added for Node and Edge, just to make sure they are used and initialised properly.

--- app/src/main/java/spe/net/navigationaltool/spe/net/navigationaltool/graph/Edge.java
+++ app/src/main/java/spe/net/navigationaltool/spe/net/navigationaltool/graph/Edge.java
@@ -5,4 +5,29 @@ public class Edge {
     public Node a;
     public Node b;
 
+    public boolean disabled;
+    public AccessLevel accessLevel;
+
+    public Edge(Node a, Node b){
+        this.a = a;
+        this.b = b;
+
+        disabled = true; // Just as an assumption, most routes are corridors.
+        accessLevel = AccessLevel.STUDENTCARD; // As an assumption, most places will require a ucard to get to.
+    }
+
+    public Edge(Node a, Node b, AccessLevel access, boolean disabledRoute){
+        this(a, b);
+
+        disabled = disabledRoute;
+        accessLevel = access;
+    }
+
+
+    public enum AccessLevel{
+        ANYONE,
+        STUDENTCARD,
+        STAFF
+    }
+
 }
--- app/src/main/java/spe/net/navigationaltool/spe/net/navigationaltool/graph/Node.java
+++ app/src/main/java/spe/net/navigationaltool/spe/net/navigationaltool/graph/Node.java
@@ -1,4 +1,15 @@
 package spe.net.navigationaltool.spe.net.navigationaltool.graph;
 
+import android.graphics.Point;
+
 public class Node {
+
+    public Point location;
+    public int floor;
+
+    public Node(Point loc, int floor){
+        this.location = loc;
+        this.floor = floor;
+    }
+
 }
