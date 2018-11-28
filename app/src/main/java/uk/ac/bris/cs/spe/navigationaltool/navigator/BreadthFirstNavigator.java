69fb14adf16fe828c4dbc1a02a7db4ad01d028a9
Laurence <hii488@btinternet.com>
2018-11-28 11:41:08 +0000

    Breadth First Navigator

---
+++ app/src/main/java/uk/ac/bris/cs/spe/navigationaltool/navigator/BreadthFirstNavigator.java
@@ -0,0 +1,112 @@
+package uk.ac.bris.cs.spe.navigationaltool.navigator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import uk.ac.bris.cs.spe.navigationaltool.graph.Graph;
+import uk.ac.bris.cs.spe.navigationaltool.graph.Location;
+import uk.ac.bris.cs.spe.navigationaltool.graph.Path;
+
+public class BreadthFirstNavigator implements Navigator {
+
+    public List<Path> navigate(Location start, Location end, Graph graph){
+
+        Tree<Location> routes = new Tree<Location>(start);
+        ArrayList<Path> pathList = new ArrayList<Path>();
+
+        // While no route has found the end
+        while(!routes.contains(end)){
+            // Go through each of the end nodes
+            for(Node<Location> node : routes.getLeafs()){
+
+                // Add each joined node on the end, forming a new 'route'
+                for(Path p : graph.getPathsFromLocation(node.data))
+                    node.addChild(p.getOtherLocation(node.data));
+
+            }
+        }
+
+        // Once 'end' is in the structure, go through all leafs (end must be a leaf) and find it.
+        for(Node<Location> node : routes.getLeafs()){
+            if(node.data.equals(end)){
+                Node currentNode = node;
+
+                // If you haven't yet reached the start location
+                while(!currentNode.data.equals(start)){
+                    Path p = null;
+
+                    // Search all paths leading from the current Node, for one that goes to the node's parent's node.
+                    for(Path possiblePath : graph.getPathsFromLocation(node.data))
+                        if(possiblePath.getOtherLocation(node.data).equals(node.parent.data))
+                            p = possiblePath;
+
+                    // Add this path to the pathList and set the parent node as the new current Node.
+                    pathList.add(p);
+                    currentNode = currentNode.parent;
+                }
+
+            }
+        }
+
+
+
+        return pathList;
+    }
+
+
+    // Quick tree implementation
+    private class Tree<T> {
+        public Node<T> root;
+
+        public Tree(T rootData) {
+            root = new Node<T>(rootData);
+        }
+
+        public boolean contains(T object){
+            return root.contains(object);
+        }
+
+        public ArrayList<Node<T>> getLeafs(){
+            return root.getLeafs();
+        }
+    }
+
+    public class Node<T> {
+        public T data;
+        public Node<T> parent;
+        public List<Node<T>> children;
+
+        public Node(T t){
+            data = t;
+            children = new ArrayList<Node<T>>();
+        }
+
+        public void addChild(T t){
+            children.add(new Node<T>(t));
+        }
+
+        public ArrayList<Node<T>> getLeafs(){
+            ArrayList<Node<T>> leafs = new ArrayList<Node<T>>();
+
+            if(children == null || children.isEmpty())
+                leafs.add(this);
+            else
+                children.forEach(c -> leafs.addAll(c.getLeafs()));
+
+            return leafs;
+        }
+
+        public boolean contains(T object){
+            if(object.equals(data)) return true;
+
+            boolean found = false;
+
+            for(Node<T> c : children){
+                if(c.contains(object)) found = true;
+            }
+
+            return found;
+        }
+    }
+
+}
--- app/src/main/java/uk/ac/bris/cs/spe/navigationaltool/navigator/Navigator.java
+++ app/src/main/java/uk/ac/bris/cs/spe/navigationaltool/navigator/Navigator.java
@@ -2,12 +2,13 @@ package uk.ac.bris.cs.spe.navigationaltool.navigator;
 
 import java.util.List;
 
+import uk.ac.bris.cs.spe.navigationaltool.graph.Graph;
 import uk.ac.bris.cs.spe.navigationaltool.graph.Location;
 import uk.ac.bris.cs.spe.navigationaltool.graph.Path;
 
 public interface Navigator {
 
     // Maybe we should pass this the graph too?
-    public List<Path> navigate(Location start, Location end);
+    public List<Path> navigate(Location start, Location end, Graph graph);
 
 }
