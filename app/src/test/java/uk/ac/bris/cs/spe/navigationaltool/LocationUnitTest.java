2ab59e827ce248072c90ba51e8a92b5cc9e974ec
Ainesh Sevellaraja <ainesh1998@outlook.com>
2018-12-05 11:42:04 +0000

    done with PathUnitTest and LocationUnitTest

--- app/src/test/java/uk/ac/bris/cs/spe/navigationaltool/LocationUnitTest.java
+++ app/src/test/java/uk/ac/bris/cs/spe/navigationaltool/LocationUnitTest.java
@@ -1,6 +1,11 @@
 package uk.ac.bris.cs.spe.navigationaltool;
 
+import android.graphics.Point;
+
 import org.junit.Test;
+
+import uk.ac.bris.cs.spe.navigationaltool.graph.Location;
+import static org.assertj.core.api.Java6Assertions.*;
 
 /**
  *  This class contains unit tests written for the Graph class' methods.
@@ -10,8 +15,28 @@ import org.junit.Test;
 public class LocationUnitTest {
 
     @Test
-    public void testNullArgumentInConstructorFails() throws Exception{
+    public void testNullArgumentInConstructorFails() throws Exception {
+        // first constructor
+        assertThatThrownBy(() -> {
+            Location loc = new Location(null, 0, "loc");
+        }).isInstanceOf(IllegalArgumentException.class);
 
+        assertThatThrownBy(() -> {
+            Location loc = new Location(new Point(0, 0), 0, null);
+        }).isInstanceOf(IllegalArgumentException.class);
+
+        // second constructor
+        assertThatThrownBy(() -> {
+            Location loc = new Location(null, 0, "loc", "Location");
+        }).isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> {
+            Location loc = new Location(new Point(0, 0), 0, null, "Location");
+        }).isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> {
+            Location loc = new Location(new Point(0, 0), 0, "loc", null);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 
 }
