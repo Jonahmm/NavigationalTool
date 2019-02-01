e9e46665a852425660e1807a12be8c42d95e4933
Laurence <hii488@btinternet.com>
2019-02-01 11:33:33 +0000

    Added default navigator method that uses location IDs
    Not sure if needed, but might help keep code clean elsewhere.

--- app/src/main/java/uk/ac/bris/cs/spe/navigationaltool/navigator/Navigator.java
+++ app/src/main/java/uk/ac/bris/cs/spe/navigationaltool/navigator/Navigator.java
@@ -7,9 +7,21 @@ import uk.ac.bris.cs.spe.navigationaltool.graph.Location;
 import uk.ac.bris.cs.spe.navigationaltool.graph.Path;
 import uk.ac.bris.cs.spe.navigationaltool.graph.User;
 
+import static java.util.Objects.requireNonNull;
+
 public interface Navigator {
 
     // Maybe we should pass this the graph too?
     public List<Path> navigate(Location start, Location end, Graph graph, User user);
 
+
+    public default List<Path> navigate(int idStart, int idEnd, Graph graph, User user){
+        Location start = graph.getLocationById(idStart);
+        Location end = graph.getLocationById(idEnd);
+
+        requireNonNull(start);
+        requireNonNull(end);
+
+        return navigate(start, end, graph, user);
+    }
 }
