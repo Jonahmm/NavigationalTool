ed78d3785321d7157b476e2196d56fbcdf12331a
Ada <lh17439@bristol.ac.uk>
2019-02-09 17:40:49 +0000

    Created SQLite database for search

---
+++ app/src/main/java/uk/ac/bris/cs/spe/navigationaltool/SQLiteHelper.java
@@ -0,0 +1,34 @@
+package uk.ac.bris.cs.spe.navigationaltool;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import uk.ac.bris.cs.spe.navigationaltool.database.DatabaseConstants;
+
+public class SQLiteHelper extends SQLiteOpenHelper implements DatabaseConstants {
+
+    public static final int DB_VERSION = 2;
+
+    public SQLiteHelper(Context context) {
+        super(context,DB_NAME, null, DB_VERSION);
+        getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_LOC + "(" + COL_LOC_ID
+                + " INTEGER PRIMARY KEY NOT NULL, " + " " + COL_LOC_NAME
+                + " VARCHAR(50) NOT NULL);");
+        insertLocation(db, "Powell");
+    }
+
+    private static void insertLocation(SQLiteDatabase db, String location) {
+        db.execSQL("INSERT INTO " + TABLE_LOC + " (" + COL_LOC_NAME
+                + ") VALUES ('" + location + "');");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+}
