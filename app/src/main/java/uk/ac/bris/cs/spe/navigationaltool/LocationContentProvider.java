e632afe84730b050064297ee99c626d83c4160ee
Ada <lh17439@bristol.ac.uk>
2019-02-09 18:00:07 +0000

    Created ContentProvider for the database

---
+++ app/src/main/java/uk/ac/bris/cs/spe/navigationaltool/LocationContentProvider.java
@@ -0,0 +1,55 @@
+package uk.ac.bris.cs.spe.navigationaltool;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+
+import uk.ac.bris.cs.spe.navigationaltool.database.DatabaseConstants;
+
+public class LocationContentProvider extends ContentProvider {
+    private static final String AUTHORITY = "uk.ac.bris.cs.spe.navigationaltool.LocationContentProvider";
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY
+            + "/" + DatabaseConstants.TABLE_LOC);
+    private SQLiteHelper dbHelper;
+
+    @Override
+    public boolean onCreate() {
+        dbHelper = new SQLiteHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+        qb.setTables(DatabaseConstants.TABLE_LOC);
+        String orderBy = DatabaseConstants.COL_LOC_NAME + " asc";
+        Cursor cursor = qb.query(dbHelper.getReadableDatabase(),
+                new String[] { DatabaseConstants.COL_LOC_ID,
+                        DatabaseConstants.COL_LOC_NAME }, null,
+                null, null, null, orderBy);
+        return cursor;
+    }
+
+    public String getType(Uri uri) {
+        return "vnd.android.cursor.dir/vnd.uk.ac.bris.cs.spe.navigationaltool.LocationContentProvider";
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        return 0;
+    }
+}
