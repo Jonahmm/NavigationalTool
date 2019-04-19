a7095e1ae1f20cb6bb34b9b1b96125fabf171f45
Ada <lh17439@bristol.ac.uk>
2019-04-19 15:37:43 +0300

    Added functions to VrView

--- app/src/main/java/uk/ac/bris/cs/spe/navigationaltool/VrView.java
+++ app/src/main/java/uk/ac/bris/cs/spe/navigationaltool/VrView.java
@@ -1,5 +1,6 @@
 package uk.ac.bris.cs.spe.navigationaltool;
 
+import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
@@ -17,7 +18,9 @@ public class VrView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vr_view);
         mVRPanoramaView = findViewById(R.id.vrPanoramaView);
+        loadPhotoSphere();
     }
+
 
     private void loadPhotoSphere(){
         VrPanoramaView.Options options = new VrPanoramaView.Options();
@@ -33,6 +36,24 @@ public class VrView extends AppCompatActivity {
             e.printStackTrace();
         }
 
+    }
+
+    @Override
+    protected void onPause() {
+        mVRPanoramaView.pauseRendering();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mVRPanoramaView.resumeRendering();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mVRPanoramaView.shutdown();
+        super.onDestroy();
     }
 
 }
